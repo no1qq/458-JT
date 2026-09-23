6ee6d97c09b308e1745a63505462f7f2e522d4e4
no1qq <business.rakid0y@gmail.com>
2026-09-23 19:35:21 +0200

    add clean.bat script to purge build artifacts

---
+++ clean.bat
@@ -0,0 +1,8 @@
+@echo off
+rem cleaning cargo target directory
+cargo clean
+if exist "target" rmdir /s /q "target" 2>nul
+rem cleaning bin directory and binaries
+if exist "bin" rmdir /s /q "bin" 2>nul
+if exist "*.old.exe" del /f /q "*.old.exe" 2>nul
+echo clean complete: build artifacts removed
